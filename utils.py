8e8d8d51c911df1fec8b84a9bbd5861621629864
AntoineMsy <42070990+AntoineMsy@users.noreply.github.com>
2022-05-19 18:01:53 +0200

    Add files via upload

---
+++ utils.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 22 15:07:38 2022
+
+@author: antoi
+"""
+import matplotlib.pyplot as plt
+import numpy as np
+import torchvision.transforms.functional as F
+
+def show(imgs):
+    if not isinstance(imgs, list):
+        imgs = [imgs]
+    fix, axs = plt.subplots(ncols=len(imgs), squeeze=False)
+    for i, img in enumerate(imgs):
+        img = img.detach()
+        img = F.to_pil_image(img)
+        axs[0, i].imshow(np.asarray(img))
+        axs[0, i].set(xticklabels=[], yticklabels=[], xticks=[], yticks=[])
